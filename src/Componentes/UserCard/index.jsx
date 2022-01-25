a3302ea5f79c3ec104bb43ab9bf0da95316814ac
facumay21 <facumay21@gmail.com>
2022-01-25 20:47:18 -0300

    cambie edad por precio que estaba mal

--- src/Componentes/UserCard/index.jsx
+++ src/Componentes/UserCard/index.jsx
@@ -6,7 +6,7 @@ function UserCard ( {product, price, comprar}) {
     return (
         <div className="card">
             <p>Nombre: {product} </p>
-            <p>Edad: {price} </p>
+            <p>Precio: {price} </p>
             <button onClick={comprar}> Compar </button>
 
         </div>
